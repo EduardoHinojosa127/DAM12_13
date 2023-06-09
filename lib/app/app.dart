e494dd53f421e507d0ba3290b5935a7872904025
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2023-06-08 22:31:43 -0500

    DAM-10

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,12 +5,22 @@ import 'package:laboratorio02/main.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const primary = Color(0xFF40B7AD);
+    const textColor = Color(0xFF4A4A4A);
+    const BackgroundColor = Color(0xFFF5F5F5);
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: primary),
+          scaffoldBackgroundColor: BackgroundColor,
+          textTheme: Theme.of(context).textTheme.apply(
+                fontFamily: 'Poppins',
+                bodyColor: textColor,
+                displayColor: textColor,
+              ),
           useMaterial3: true,
         ),
         home: SplashPage());
--- lib/app/view/splash.dart
+++ lib/app/view/splash.dart
@@ -3,144 +3,199 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double marginPercentage =
+        0.2; // Ajusta este valor para establecer el margen deseado
+
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF448AFF),
-                Color(0xFF0D47A1),
-              ],
+      appBar: AppBar(
+        title: Text('Mi Aplicativo'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Noticias',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
             ),
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2.0,
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Image.asset(
-                          'assets/carpincho.jpg',
-                          width: 300,
-                          height: 300,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        '¡Bienvenido!',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Esta es mi página web',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+            ListTile(
+              title: Text(
+                '"Descubren una nueva especie de flores luminosas en el Amazonas"',
+                style: TextStyle(
+                    fontFamily: 'Poppins', fontWeight: FontWeight.w700),
               ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/image1.jpg',
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
-                          ),
-                          SizedBox(width: 16),
-                          Expanded(
-                            child: Text(
-                              'Linux',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        'Linux es un sistema operativo de código abierto que ofrece flexibilidad y personalización. Es altamente utilizado en servidores y en el ámbito de la programación y desarrollo de software. Proporciona un entorno de línea de comandos potente y una gran variedad de herramientas y software de código abierto.',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(height: 32),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Windows',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 16),
-                          Image.asset(
-                            'assets/image2.jpg',
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        'Windows, por otro lado, es un sistema operativo desarrollado por Microsoft. Es ampliamente utilizado en computadoras personales y ofrece una interfaz gráfica intuitiva y fácil de usar. Windows es compatible con una amplia gama de aplicaciones y juegos, y está diseñado para satisfacer las necesidades del usuario promedio.',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+              subtitle: Text(
+                  'Investigadores botánicos han hecho un emocionante hallazgo en el corazón de la selva amazónica. Se trata de una especie de flores que emiten una suave luz, creando un espectáculo natural único. Este descubrimiento podría tener implicaciones tanto en la biología como en la iluminación sostenible.'),
+              onTap: () {
+                // Aquí puedes agregar la funcionalidad para la noticia 1
+              },
+            ),
+            ListTile(
+              title: Text(
+                '"Joven emprendedor revoluciona la industria de la moda con ropa reciclada"',
+                style: TextStyle(
+                    fontFamily: 'Poppins', fontWeight: FontWeight.w700),
               ),
-            ],
+              subtitle: Text(
+                  'Un talentoso emprendedor ha encontrado la forma de combinar moda y sostenibilidad al crear una línea de ropa exclusiva utilizando materiales reciclados. Su innovador enfoque ha captado la atención de la industria y está cambiando la forma en que vemos la moda, demostrando que es posible ser elegante y respetar el medio ambiente al mismo tiempo.'),
+              onTap: () {
+                // Aquí puedes agregar la funcionalidad para la noticia 1
+              },
+            ),
+            ListTile(
+              title: Text(
+                '"Investigadores logran avance en la lucha contra el cáncer"',
+                style: TextStyle(
+                    fontFamily: 'Poppins', fontWeight: FontWeight.w700),
+              ),
+              subtitle: Text(
+                  'Científicos y médicos han dado un paso significativo en la batalla contra el cáncer al descubrir un nuevo enfoque terapéutico. Mediante el uso de una combinación de terapias personalizadas y tecnología de vanguardia, han logrado mejorar las tasas de supervivencia y ofrecer nuevas esperanzas a los pacientes y sus familias.'),
+              onTap: () {
+                // Aquí puedes agregar la funcionalidad para la noticia 1
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade900, Colors.blue.shade400],
           ),
         ),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 40),
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 40),
+                Image.asset(
+                  'assets/images/bienvenida1.jpeg',
+                  width: 250,
+                  height: 250,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  '¡Bienvenido!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: screenWidth * marginPercentage),
+                  child: Text(
+                    'En nuestro aplicativo, te damos la más cordial bienvenida a una experiencia única para simplificar tu vida. Nuestra misión es ayudarte a organizar tus tareas y aumentar tu productividad de manera eficiente. ¡Descubre todas las funcionalidades que tenemos para ofrecerte!',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 80),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 40),
+                Image.asset(
+                  'assets/images/bienvenida2.jpg',
+                  width: 250,
+                  height: 250,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Descubre nuevas funcionalidades',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: screenWidth * marginPercentage),
+                  child: Text(
+                    'En constante evolución, nuestro aplicativo se adapta a tus necesidades y te sorprenderá con nuevas funcionalidades. Desde la integración con calendarios y recordatorios hasta la posibilidad de compartir tareas con otros usuarios, estamos comprometidos en brindarte la mejor experiencia. Nuestro equipo de desarrollo trabaja arduamente para ofrecerte actualizaciones periódicas, asegurándose de que siempre estés al tanto de las últimas herramientas y mejoras.',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 80),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 40),
+                Image.asset(
+                  'assets/images/bienvenida3.jpeg',
+                  width: 250,
+                  height: 250,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Aumenta tu productividad',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: screenWidth * marginPercentage),
+                  child: Text(
+                    'En un mundo lleno de distracciones, es fundamental contar con herramientas que te permitan maximizar tu productividad. Con nuestro aplicativo, podrás llevar un registro detallado de tus tareas, establecer recordatorios y marcar tus progresos a medida que avanzas hacia tus metas. No importa si se trata de un proyecto laboral, un estudio académico o simplemente las actividades diarias, nuestro aplicativo te brindará la estructura necesaria para mantener el enfoque y alcanzar tus objetivos de manera eficiente.',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Nosotros',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Contáctanos',
+          ),
+        ],
       ),
     );
   }
